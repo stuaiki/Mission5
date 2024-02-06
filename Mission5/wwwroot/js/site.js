f3f75b576adbe3658b9159faafb5184de146a3a4
stuaiki <takaku@byu.edu>
2024-02-05 22:35:45 -0700

    Add project files.

---
+++ Mission5/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$('#calcTotal').click( function () {
+    let hours = parseInt($('#hours').val());
+    if (hours > 0) {
+        let totalCost = hours * 30;
+
+        $('#totalCost').val("$" + totalCost);
+    }
+    else {
+        alert("Enter the valid number.")
+        $("#hours").val("");
+        $('#totalCost').val("");
+    }
+})
