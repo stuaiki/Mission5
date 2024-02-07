75f4b1f6a074795180104c61b3ec6d368c6bfd25
stuaiki <takaku@byu.edu>
2024-02-07 14:37:19 -0700

    chaged

--- Mission5/wwwroot/js/site.js
+++ Mission5/wwwroot/js/site.js
@@ -2,6 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// This is jQuery. Based on id in cshtml, making methods
+// Create valuable named hours and based on what user input, change the output.
+// If user input valid value, it return total cost, and if user input invalid value, clear all boxes and show alert message.
 $('#calcTotal').click( function () {
     let hours = parseInt($('#hours').val());
     if (hours > 0) {
